e34b5cb35dae9ca6db314ffa4c9ca0a006675b06
aluvon <aluvon5089@ieselcaminas.org>
2023-12-14 09:47:22 +0100

    ejercicio1

---
+++ ejercicio1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Ingresa el primer numero: $1"
+echo "Ingresa el segundo numero: $2"
+if [[ $1 -gt $2 ]]; then
+	echo "El numero $1 es mayor que el numero $2"
+else
+	echo "El numero $2 es mayor que el numero $1"
+fi
