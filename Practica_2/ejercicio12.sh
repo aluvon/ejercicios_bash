2bc0cd1e9f62a8c3a964e2c3fa013e9a341d2575
aluvon <aluvon5089@ieselcaminas.org>
2024-01-03 10:54:20 +0100

    practica_2

--- Practica_2/ejercicio12.sh
+++ Practica_2/ejercicio12.sh
@@ -16,15 +16,13 @@ function mult(){
 function salir(){
 	exit
 }
-echo "Hola, en eread -p "Igresa el primer valor " valor1
 
-read -p "INgresa el segundo valor " valor2ste script realizare las operaciones matematicas"
-echo "Selecciona la operacion que deseas realizar:"
-echo "1 Sumar"
-echo "2 Restar"
-echo "3 Dividir"
-echo "4 Multiplicar"
-echo "0 Salir"
+echo "Selecciona la operacion que deseas realizar: "
+echo "1 Sumar."
+echo "2 Restar."
+echo "3 Dividir."
+echo "4 Multiplicar."
+echo "0 Salir."
 echo ""
 
 read -p "Ingresa la opcion: " opcion
@@ -32,8 +30,8 @@ if [[ $opcion -eq 0 ]]; then
 	salir
 
 elif [[ $opcion -ne 0 ]]; then
-	read -p "Igresa el primer valor " valor1
-	read -p "Ingresa el segundo valor " valor2
+	read -p "Igresa el primer valor: " valor1
+	read -p "Ingresa el segundo valor: " valor2
 
 		case $opcion in
 		1)
